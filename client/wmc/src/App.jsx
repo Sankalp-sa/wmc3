a19f30c0a2a7db7f0f142ae27f40718e919de007
intelkid7 <priyanshjsyadav777@gmail.com>
2023-07-03 15:52:41 +0530

    hello

--- client/wmc/src/App.jsx
+++ client/wmc/src/App.jsx
@@ -4,7 +4,25 @@ import './App.css'
 function App() {
   return (
     <>
-      
+      <div>
+        <div>
+          <nav className="navbar">
+            <div className="logo"><img src="HogwartsLogo.png" alt /></div>
+            <ul className="navlist">
+              <li><a href="#">Home</a></li>
+              <li><a href="#">About Us</a></li>
+              <li><a href="#" id="cu">Contact Us</a></li>
+              <div />
+            </ul>
+            <div className="searchbar">
+              <input className="search" type="text" id="srch" />
+              <button type="submit" className="srchbtn">Search</button>
+            </div>
+          </nav>
+        </div>
+        <canvas />
+      </div>
+
     </>
   )
 }
